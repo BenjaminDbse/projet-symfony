789341f258b0fea7492804fd9c4fbe16f8ffec88
BenjaminDbse <benjideboise@gmail.com>
2020-12-01 17:51:34 +0100

    quest 08

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Program;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,8 +20,12 @@ class ProgramController extends AbstractController
      */
     public function index(): Response
     {
+        $programs = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findAll();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 
@@ -31,6 +36,15 @@ class ProgramController extends AbstractController
      */
     public function show(int $id = 1): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $this->getDoctrine()
+            ->getRepository(Program::class)
+            ->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', ['program' => $program]);
     }
 }
